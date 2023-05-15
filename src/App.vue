468e25f9f7601beda7db9e7742f2686de5191186
AnrelWsh <hmeliangel11@gmail.com>
2023-05-15 11:52:13 +0200

    Init project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Header msg="header" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <h1 class="text-3xl font-bold underline font-2xl">
+    Hello world!
+  </h1>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Header
+  }
+}
+</script>
