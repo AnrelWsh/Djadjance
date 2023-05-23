33351e0a6c60afe1810a597ef18043239f36dae1
AnrelWsh <hmeliangel11@gmail.com>
2023-05-23 20:07:41 +0200

    Responsive Projects' Cards

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,17 @@
 <template>
-  <nav class="flex flex-row items-center text-[24px] border-b-4 border-gris select-none">
+  <nav class="flex flex-row items-stretch text-[24px] border-b-4 border-gris select-none">
     <div class="flex flex-row items-center w-[70vw] py-4">
       <a href="">
-        <img class="w-[5vw] mx-32" src="../assets/Logo/Logo.png" alt="logo">  
+        <img class="w-[7vw] lg:mx-32 mx-8" src="../assets/Logo/Logo.png" alt="logo">  
       </a>
-      <div class="flex flex-row gap-16 px-16">
+      <div class="flex flex-row sm:gap-16 gap-4 lg:px-16 px-8">
         <router-link class="navTitle" to="/accueil">Accueil</router-link>
         <router-link class="navTitle" to="/projets">Projets</router-link>
         <router-link class="navTitle" to="/apropos">A propos</router-link>
       </div>
     </div>
 
-    <div class="w-[4px] h-[16vh] bg-gris"></div>
+    <div class="w-[4px] bg-gris"></div>
    
     <div class="m-auto py-4">
       <router-link class="contactTitle" to="/contact">Me contacter</router-link>
--- src/components/HomePrez.vue
+++ src/components/HomePrez.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex flex-row border-b-4 border-gris h-[54.5vh]">
-    <div class=" flex flex-col justify-center w-[50vw] p-24">
-      <h2 class="text-[32px]">BIENVENUE SUR MON SITE,</h2>
-      <p class="grotesk-reg text-[32px]">Bonjour, je m'appelle Yanis Djadja, je suis étudiant à l'IIM en direction artistique, Interactivité et UX/UI !</p>
+    <div class=" flex flex-col justify-center lg:w-[50vw] w-[70vw] md:p-24 p-12 lg:text-[32px] text-[24px]">
+      <h2>BIENVENUE SUR MON SITE,</h2>
+      <p class="grotesk-reg">Bonjour, je m'appelle Yanis Djadja, je suis étudiant à l'IIM en direction artistique, Interactivité et UX/UI !</p>
     </div>
 
     <div class="w-[4px] bg-gris"></div>
 
-    <div class="w-[50vw] bg-mauveClair"></div>
+    <div class="lg:w-[50vw] w-[30vw] bg-mauveClair"></div>
   </div>
 </template>
 
